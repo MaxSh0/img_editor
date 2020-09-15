471681ad105589e5f7d73178675f5e7a895086e7
Maxim Shakurov <m.shakurov99@mail.ru>
2020-09-15 12:13:50 +0400

    6 methods

---
+++ lab1Game/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace lab1Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonGrey = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(972, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Enabled = false;
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(462, 372);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonGrey
+            // 
+            this.buttonGrey.Location = new System.Drawing.Point(12, 435);
+            this.buttonGrey.Name = "buttonGrey";
+            this.buttonGrey.Size = new System.Drawing.Size(189, 22);
+            this.buttonGrey.TabIndex = 2;
+            this.buttonGrey.Text = "Сделать черно-белым";
+            this.buttonGrey.UseVisualStyleBackColor = true;
+            this.buttonGrey.Click += new System.EventHandler(this.buttonGrey_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(496, 43);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(462, 372);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Увеличить контрасность на 50 %";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 492);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Замена каналов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(285, 435);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 22);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Поворот 90 градусов";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(285, 464);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(189, 22);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Поворот 180 градусов";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(285, 493);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(189, 22);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Размытие по Гауссу";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(972, 551);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.buttonGrey);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lab1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonGrey;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ lab1Game/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            сохранитьКакToolStripMenuItem.Enabled = false;
+            OpenFileDialog Fdia = new OpenFileDialog();
+            Fdia.Filter = "Image Files(*.BMP; *.JPG; *.GIF; *.PNG)| *.BMP; *.JPG; *.GIF; *.PNG | All files(*.*) | *.* ";
+            if (Fdia.ShowDialog() == DialogResult.OK) 
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(Fdia.FileName);
+                }
+                catch 
+                {
+                    MessageBox.Show("Невозможно открыть данный файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null) 
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Сохранить картинку как...";
+                sfd.OverwritePrompt = true;
+                sfd.CheckPathExists = true; 
+                sfd.Filter = "Image Files(*.JPG; *.GIF; *.PNG)| *.BMP; *.JPG; *.GIF; *.PNG | All files(*.*) | *.* ";
+                sfd.ShowHelp = true; 
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox2.Image.Save(sfd.FileName);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        private void buttonGrey_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap InputImage = new Bitmap(pictureBox1.Image);
+                Bitmap OutputImage = new Bitmap(InputImage.Width, InputImage.Height);
+                for (int i = 0; i < InputImage.Width; i++) 
+                {
+                    for (int j = 0; j < InputImage.Height; j++) 
+                    {
+                        UInt32 pixel = (UInt32)(InputImage.GetPixel(i, j).ToArgb());
+
+                        float R = (float)((pixel & 0x00FF0000) >> 16); // красный
+                        float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
+                        float B = (float)(pixel & 0x000000FF); // синий
+
+                        R = G = B = (R + G + B) / 3.0f;
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                        OutputImage.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+                }
+                pictureBox2.Image = OutputImage;
+                сохранитьКакToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Картинка не загружена", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             Увеличение контрастности на N процентов:
+             I = (I • 100 – 128 • N) / (100 – N)
+             I – соответственно R, G, B каналы каждой точки изображения.
+             Если I меньше 0 или больше 255 обрезаем
+             */
+            if (pictureBox1.Image != null)
+            {
+                Bitmap InputImage = new Bitmap(pictureBox1.Image);
+                Bitmap OutputImage = new Bitmap(InputImage.Width, InputImage.Height);
+                for (int i = 0; i < InputImage.Width; i++)
+                {
+                    for (int j = 0; j < InputImage.Height; j++)
+                    {
+                        UInt32 pixel = (UInt32)(InputImage.GetPixel(i, j).ToArgb());
+                        double R = (double)((pixel & 0x00FF0000) >> 16); // красный
+                        double G = (double)((pixel & 0x0000FF00) >> 8); // зеленый
+                        double B = (double)(pixel & 0x000000FF); // синий
+
+                        R = (R * 100 - 128 * 50) / (100 - 50);
+                        if (R > 255)
+                        {
+                            R = 255;
+                        }
+                        else if (R < 0) 
+                        {
+                            R = 0;
+                        }
+                        G = (G * 100 - 128 * 50) / (100 - 50);
+                        if (G > 255)
+                        {
+                            G = 255;
+                        }
+                        else if (G < 0)
+                        {
+                            G = 0;
+                        }
+                        B = (B * 100 - 128 * 50) / (100 - 50);
+                        if (B > 255)
+                        {
+                            B = 255;
+                        }
+                        else if (B < 0)
+                        {
+                            B = 0;
+                        }
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                        OutputImage.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+                }
+                pictureBox2.Image = OutputImage;
+                сохранитьКакToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Картинка не загружена", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap InputImage = new Bitmap(pictureBox1.Image);
+                Bitmap OutputImage = new Bitmap(InputImage.Width, InputImage.Height);
+                for (int i = 0; i < InputImage.Width; i++)
+                {
+                    for (int j = 0; j < InputImage.Height; j++)
+                    {
+                        UInt32 pixel = (UInt32)(InputImage.GetPixel(i, j).ToArgb());
+
+                        float R = (float)((pixel & 0x00FF0000) >> 16); // красный
+                        float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
+                        float B = (float)(pixel & 0x000000FF); // синий
+
+                        R = G;
+                        G = B;
+                        B = R;
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                        OutputImage.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+                }
+                pictureBox2.Image = OutputImage;
+                сохранитьКакToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Картинка не загружена", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap InputImage = new Bitmap(pictureBox1.Image);
+            Bitmap OutputImage = new Bitmap(InputImage.Width, InputImage.Height);
+            OutputImage = InputImage;
+            OutputImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox2.Image = OutputImage;
+            сохранитьКакToolStripMenuItem.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap InputImage = new Bitmap(pictureBox1.Image);
+            Bitmap OutputImage = new Bitmap(InputImage.Width, InputImage.Height);
+            OutputImage = InputImage;
+            OutputImage.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            pictureBox2.Image = OutputImage;
+            сохранитьКакToolStripMenuItem.Enabled = true;
+        }
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+
+        //размыть
+        private void button5_Click(object sender, EventArgs e)
+        {
+            UInt32[,] pixel;
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            Bitmap OutputImage = new Bitmap(image.Width, image.Height);
+            pixel = new UInt32[image.Height, image.Width];
+            for (int y = 0; y < image.Height; y++)
+                for (int x = 0; x < image.Width; x++)
+                    pixel[y, x] = (UInt32)(image.GetPixel(x, y).ToArgb());
+            pixel = matrix_filtration(image.Width, image.Height, pixel, N2, blur);
+            pictureBox2.Image = FromPixelToBitmap(OutputImage,pixel);
+        }
+
+        //преобразование из UINT32 to Bitmap
+        public static Bitmap FromPixelToBitmap(Bitmap image, UInt32[,] pixel)
+        {
+            for (int y = 0; y < image.Height; y++)
+                for (int x = 0; x < image.Width; x++)
+                    image.SetPixel(x, y, Color.FromArgb((int)pixel[y, x]));
+            return image;
+        }
+
+
+
+
+
+
+
+
+
+
+        struct RGB
+        {
+            public float R;
+            public float G;
+            public float B;
+        }
+
+
+        UInt32[,] matrix_filtration(int W, int H, UInt32[,] pixel, int N, double[,] matryx)
+        {
+
+
+            int i, j, k, m, gap = (int)(N / 2);
+            int tmpH = H + 2 * gap, tmpW = W + 2 * gap;
+            UInt32[,] tmppixel = new UInt32[tmpH, tmpW];
+            UInt32[,] newpixel = new UInt32[H, W];
+            //заполнение временного расширенного изображения
+            //углы
+            for (i = 0; i < gap; i++)
+                for (j = 0; j < gap; j++)
+                {
+                    tmppixel[i, j] = pixel[0, 0];
+                    tmppixel[i, tmpW - 1 - j] = pixel[0, W - 1];
+                    tmppixel[tmpH - 1 - i, j] = pixel[H - 1, 0];
+                    tmppixel[tmpH - 1 - i, tmpW - 1 - j] = pixel[H - 1, W - 1];
+                }
+            //крайние левая и правая стороны
+            for (i = gap; i < tmpH - gap; i++)
+                for (j = 0; j < gap; j++)
+                {
+                    tmppixel[i, j] = pixel[i - gap, j];
+                    tmppixel[i, tmpW - 1 - j] = pixel[i - gap, W - 1 - j];
+                }
+            //крайние верхняя и нижняя стороны
+            for (i = 0; i < gap; i++)
+                for (j = gap; j < tmpW - gap; j++)
+                {
+                    tmppixel[i, j] = pixel[i, j - gap];
+                    tmppixel[tmpH - 1 - i, j] = pixel[H - 1 - i, j - gap];
+                }
+            //центр
+            for (i = 0; i < H; i++)
+                for (j = 0; j < W; j++)
+                    tmppixel[i + gap, j + gap] = pixel[i, j];
+            //применение ядра свертки
+            RGB ColorOfPixel = new RGB();
+            RGB ColorOfCell = new RGB();
+            for (i = gap; i < tmpH - gap; i++)
+                for (j = gap; j < tmpW - gap; j++)
+                {
+                    ColorOfPixel.R = 0;
+                    ColorOfPixel.G = 0;
+                    ColorOfPixel.B = 0;
+                    for (k = 0; k < N; k++)
+                        for (m = 0; m < N; m++)
+                        {
+                            ColorOfCell = calculationOfColor(tmppixel[i - gap + k, j - gap + m], matryx[k, m]);
+                            ColorOfPixel.R += ColorOfCell.R;
+                            ColorOfPixel.G += ColorOfCell.G;
+                            ColorOfPixel.B += ColorOfCell.B;
+                        }
+                    //контролируем переполнение переменных
+                    if (ColorOfPixel.R < 0) ColorOfPixel.R = 0;
+                    if (ColorOfPixel.R > 255) ColorOfPixel.R = 255;
+                    if (ColorOfPixel.G < 0) ColorOfPixel.G = 0;
+                    if (ColorOfPixel.G > 255) ColorOfPixel.G = 255;
+                    if (ColorOfPixel.B < 0) ColorOfPixel.B = 0;
+                    if (ColorOfPixel.B > 255) ColorOfPixel.B = 255;
+
+                    newpixel[i - gap, j - gap] = build(ColorOfPixel);
+                }
+
+            return newpixel;
+        }
+
+        //вычисление нового цвета
+        RGB calculationOfColor(UInt32 pixel, double coefficient)
+        {
+            RGB Color = new RGB();
+            Color.R = (float)(coefficient * ((pixel & 0x00FF0000) >> 16));
+            Color.G = (float)(coefficient * ((pixel & 0x0000FF00) >> 8));
+            Color.B = (float)(coefficient * (pixel & 0x000000FF));
+            return Color;
+        }
+
+        //сборка каналов
+        UInt32 build(RGB ColorOfPixel)
+        {
+            UInt32 Color;
+            Color = 0xFF000000 | ((UInt32)ColorOfPixel.R << 16) | ((UInt32)ColorOfPixel.G << 8) | ((UInt32)ColorOfPixel.B);
+            return Color;
+        }
+
+
+
+
+
+
+
+
+        //повышение резкости
+        public const int N1 = 3;
+        public static double[,] sharpness = new double[N1, N1] {{-1, -1, -1},
+                                                               {-1,  9, -1},
+                                                               {-1, -1, -1}};
+
+        //размытие
+        public const int N2 = 3;
+        public static double[,] blur = new double[N1, N1] {{0.111, 0.111, 0.111},
+                                                               {0.111, 0.111, 0.111},
+                                                               {0.111, 0.111, 0.111}};
+
+    }
+}
